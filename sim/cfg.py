fe4343457f38acdd8643ae4f3277b1cf780c065f
Sam Neymotin <samnemo@gmail.com>
2022-02-28 16:20:50 -0500

    restore current dipole recording

--- sim/cfg.py
+++ sim/cfg.py
@@ -148,14 +148,14 @@ cfg.recordStep = 0.1
 
 # setting up current dipole recordings
 #  based on https://github.com/NathanKlineInstitute/A1/blob/salva_layers/cfg.py
-# cfg.recordDipole = True
-# cfg.saveDipoleCells = ['all']
-# cfg.saveDipolePops = cfg.allpops
+cfg.recordDipole = True
+cfg.saveDipoleCells = ['all']
+cfg.saveDipolePops = cfg.allpops
 
 #------------------------------------------------------------------------------
 # Saving
 #------------------------------------------------------------------------------
-cfg.simLabel = 'v6_batch0_25feb22'
+cfg.simLabel = 'v6_batch0_28feb22'
 cfg.saveFolder = '../data/'+cfg.simLabel
 # cfg.filename =                	## Set file output name
 cfg.savePickle = True         	## Save pkl file
@@ -181,7 +181,7 @@ cfg.analysis['plotTraces'] = {'include': cfg.recordCells, 'oneFigPer': 'cell', '
 
 # save dipole figure                            
 #  from https://github.com/NathanKlineInstitute/A1/blob/salva_layers/cfg.py                            
-# cfg.analysis['plotDipole'] = {'saveFig': True} 
+cfg.analysis['plotDipole'] = {'saveFig': True} 
 
 #------------------------------------------------------------------------------
 # Network 
