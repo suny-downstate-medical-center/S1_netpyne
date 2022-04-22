fd206278050e060256df66e6cdf6ebad8599a3ac
fernando <fernandodasilvaborges@gmail.com>
2022-04-22 17:47:10 -0300

    t15s

--- sim/load_plot.py
+++ sim/load_plot.py
@@ -9,74 +9,129 @@ import IPython as ipy
 import pickle as pkl
 
 
-###########################
-######## MAIN CODE ########
-###########################
 
-#------------------------------------------------------------------------------  
-# S1 Cells
-#------------------------------------------------------------------------------  
-# Load 55 Morphological Names and Cell pop numbers -> L1:6 L23:10 L4:12 L5:13 L6:14
-# Load 207 Morpho-electrical Names used to import the cells from 'cell_data/' -> L1:14 L23:43 L4:46 L5:52 L6:52
-
-poptypeNumberS1 = 55 # max 55
-celltypeNumberS1 = 207 # max 207
+poptypeNumber = 61 # max 55 + 6
+celltypeNumber = 213 # max 207 + 6
 
 # TO DEBUG - import and simulate only the Cell soma (to study only the Net)
-reducedtestS1 = True    
+reducedtest = False    
+
+#------------------------------------------------------------------------------  
+#------------------------------------------------------------------------------  
+# S1 Cells
+# Load 55 Morphological Names and Cell pop numbers -> L1:6 L23:10 L4:12 L5:13 L6:14
+# Load 207 Morpho-electrical Names used to import the cells from 'cell_data/' -> L1:14 L23:43 L4:46 L5:52 L6:52
+# Create [Morphological,Electrical] = number of cell metype in the sub-pop
 
 with open('../info/anatomy/S1-cells-distributions-Rat.txt') as mtype_file:
     mtype_content = mtype_file.read()       
 
-popNumberS1 = {}
-cellNumberS1 = {} 
-popLabelS1 = {} 
-popParamS1 = []
-cellParamS1 = []
-meParamLabelsS1 = {} 
-popLabelElS1 = {} 
-cellLabelS1 = {}
+popNumber = {}
+cellNumber = {} 
+popLabel = {} 
+popParam = []
+cellParam = []
+meParamLabels = {} 
+popLabelEl = {} 
+cellLabel = {}
+
+RP_L13 = []
+RP_L45 = []
+RP_L6 = []
 
 for line in mtype_content.split('\n')[:-1]:
     cellname, mtype, etype, n, m = line.split()
     metype = mtype + '_' + etype[0:3]
-    cellNumberS1[metype] = int(n)
-    popLabelS1[metype] = mtype
-    popNumberS1[mtype] = int(m)
-    cellLabelS1[metype] = cellname
+    cellNumber[metype] = int(n)
+    popLabel[metype] = mtype
+    popNumber[mtype] = int(m)
+    cellLabel[metype] = cellname
 
-    if mtype not in popParamS1:
-        popParamS1.append(mtype)
-        popLabelElS1[mtype] = [] 
+    if mtype not in popParam:
+        popParam.append(mtype)
+        popLabelEl[mtype] = [] 
                
-    popLabelElS1[mtype].append(metype)
+    popLabelEl[mtype].append(metype)
     
-    cellParamS1.append(mtype + '_' + etype[0:3])
+    cellParam.append(mtype + '_' + etype[0:3])
+
+    layernumber = float(metype[1:2])
+    if cellNumber[metype]*0.01 > 0.0:
+        if int(layernumber) <= 3:
+            RP_L13.append(mtype + '_' + etype[0:3])
+            # print(layernumber,int(layernumber),mtype + '_' + etype[0:3])
+        elif int(layernumber) == 6:
+            RP_L6.append(mtype + '_' + etype[0:3])
+            # print(layernumber,int(layernumber),mtype + '_' + etype[0:3])
+        else:
+            RP_L45.append(mtype + '_' + etype[0:3])
+            # print(layernumber,int(layernumber),mtype + '_' + etype[0:3])
     
-#------------------------------------------------------------------------------  
-# Thalamic Cells
-
-thalamicpops = ['ss_RTN_o', 'ss_RTN_m', 'ss_RTN_i', 'VPL_sTC', 'VPM_sTC', 'POm_sTC_s1']
+S1pops = popParam[0:55]
+S1cells = cellParam[0:207]
 
 
-for mtype in thalamicpops: # No diversity
-	metype = mtype
-	popParamS1.append(mtype)
-	cellParamS1.append(metype)
+###########################
+######## MAIN CODE ########
+###########################
 
-    
 if __name__ == '__main__':
 
     dataType = 'spont' #'speech' #'spont'
 
     if dataType == 'spont':
-        timeRange = [0, 5000]
-        filenames = ['../data/v7_batch1/v7_batch1_%d_%d_data.pkl' % (iseed, cseed) for iseed in [0] for cseed in [0]] 
+        filenames = ['../data/v7_batch1/v7_batch1_%d_%d_data.pkl' % (iseed, cseed) for iseed in [0] for cseed in [0]]
+        timeRange = [0, 15000]
+
+    layer_bounds= {'L1': 100, 'L2': 160, 'L3': 950, 'L4': 1250, 'L5A': 1334, 'L5B': 1550}#, 'L6': 2000}
+    layer_bounds= {'S': 950, 'G': 1250, 'I': 1900}#, 'L6': 2000}
+
 
     allData = []
 
     for filename in filenames:
-        sim.load(filename, instantiate=False)
+        sim.load(filename, instantiate = False)
+        # sim.load(filename, instantiate=True, instantiateConns = False, instantiateStims = False, instantiateRxD = False, createNEURONObj = False)
 
         # standardd plots
-        sim.analysis.plotRaster(**{'include': cellParamS1, 'saveFig': filename[:-8]+'_raster_S1TH', 'showFig': False, 'popRates': 'minimal', 'orderInverse': True, 'timeRange': [0,5000], 'figSize': (24,12), 'lw': 0.3, 'markerSize': 3, 'marker': '.', 'dpi': 300})
+        # sim.analysis.plotRaster(**{'include': ['allCells'], 'saveFig': True, 'showFig': False, 'labels': None, 'popRates': False,'orderInverse': True, 'timeRange': timeRange, 'figSize': (36,24), 'fontSize':4, 'lw': 5, 'markerSize':10, 'marker': '.', 'dpi': 300})
+        # sim.analysis.plotRaster(**{'include': RP_L13, 'saveFig': True, 'showFig': False, 'popRates': 'minimal', 'orderInverse': True, 'timeRange': timeRange, 'orderBy':'y', 'fontSize':8, 'figSize': (24,12), 'lw': 4.0, 'markerSize': 4, 'marker': 'o', 'dpi': 300})
+        # timeRange = [10000, 15000]
+        sim.analysis.plotRaster(**{'saveFig': True, 'showFig': False, 'labels': None, 'popRates': False, 'orderInverse': True, 'timeRange': timeRange, 'orderBy':'y', 'fontSize':4, 'figSize': (60,30), 'lw': 1.0, 'markerSize': 1, 'marker': 'o', 'dpi': 300})
+        # timeRange = [0, 5000]
+        # sim.analysis.plotRaster(**{'include': RP_L6, 'saveFig': True, 'showFig': False, 'popRates': 'minimal', 'orderInverse': True, 'timeRange': timeRange, 'orderBy':'y', 'fontSize':8, 'figSize': (48,24), 'lw': 2.0, 'markerSize': 2, 'marker': 'o', 'dpi': 300})
+        # sim.analysis.plotRaster(**{'include': RP_L6, 'saveFig': filename[:-4]+'_RP_L6', 'showFig': False, 'popRates': 'minimal', 'orderInverse': True, 'timeRange': timeRange, 'orderBy':'y', 'fontSize':8, 'figSize': (24,12), 'lw': 4.0, 'markerSize': 4, 'marker': 'o', 'dpi': 300})
+        # sim.analysis.plotSpikeStats(stats=['rate'],figSize = (6,12), timeRange=[1500, 31500], dpi=300, showFig=0, saveFig=filename[:-4]+'_stats_30sec')
+        #sim.analysis.plotSpikeStats(stats=['rate'],figSize = (6,12), timeRange=[1500, 6500], dpi=300, showFig=0, saveFig=filename[:-4]+'_stats_5sec')
+        #sim.analysis.plotLFP(**{'plots': ['spectrogram'], 'electrodes': ['avg', [0], [1], [2,3,4,5,6,7,8,9], [10, 11, 12], [13], [14, 15], [16,17,18,19]], 'timeRange': timeRange, 'maxFreq': 50, 'figSize': (8,24), 'saveData': False, 'saveFig': filename[:-4]+'_LFP_spec_7s_all_elecs', 'showFig': False})
+        # sim.analysis.plotRaster(**{'include': ['ss_RTN_o', 'ss_RTN_m', 'ss_RTN_i', 'VPL_sTC', 'VPM_sTC', 'POm_sTC_s1'], 'saveFig': True, 'showFig': False, 'popRates': 'minimal', 'orderInverse': True, 'timeRange': timeRange, 'orderBy':'y', 'fontSize':8, 'figSize': (48,24), 'lw': 2.0, 'markerSize': 2, 'marker': 'o', 'dpi': 300})
+        # sim.analysis.plotRaster(**{'include': S1cells, 'saveFig': True, 'showFig': False, 'labels': None, 'popRates': False,'orderInverse': True, 'timeRange': timeRange, 'figSize': (36,24), 'fontSize':4, 'lw': 5, 'markerSize':10, 'marker': '.', 'dpi': 300})
+        
+        # sim.analysis.plotLFP(**{'plots': ['locations'], 
+        #         'figSize': (24,24), 
+        #         'saveData': False, 
+        #         'saveFig': True, 'showFig': False, 'dpi': 300})
+
+        # sim.analysis.plotLFP(**{'plots': ['timeSeries'], 
+        #         # 'electrodes': 
+        #         # [[0,1,2,3]], #'avg', 
+        #         'timeRange': timeRange, 
+        #         'figSize': (24,12), 'saveFig': True, 'showFig': False})
+
+        # sim.analysis.plotLFP(**{'plots': ['spectrogram'], 
+        #         # 'electrodes': 
+        #         # [[0,1,2,3]],
+        #         'timeRange': timeRange, 
+        #         'maxFreq': 400, 
+        #         'figSize': (16,12), 
+        #         'saveData': False, 
+        #         'saveFig': True, 'showFig': False})
+
+        # sim.analysis.plotLFP(**{'plots': ['PSD'], 
+        #         # 'electrodes': 
+        #         # [[0,1,2,3]],
+        #         'timeRange': timeRange, 
+        #         'maxFreq': 400, 
+        #         'figSize': (8,12), 
+        #         'saveData': False, 
+        #         'saveFig': True, 'showFig': False})
---
+++ sim/saverasterandpositions.py
@@ -0,0 +1,137 @@
+
+"""
+.py
+
+High-level specifications for S1 network model using NetPyNE
+
+Contributors: fernandodasilvaborges@gmail.com
+"""
+
+from netpyne import specs
+import pickle, json
+import os
+import numpy as np
+import pandas as pd
+
+netParams = specs.NetParams()   # object of class NetParams to store the network parameters
+
+
+try:
+    from __main__ import cfg  # import SimConfig object with params from parent module
+except:
+    from cfg import cfg
+
+#------------------------------------------------------------------------------
+#
+# PARAMETERS
+#
+#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------  
+#------------------------------------------------------------------------------  
+# S1 Cells
+# Load 55 Morphological Names and Cell pop numbers -> L1:6 L23:10 L4:12 L5:13 L6:14
+# Load 207 Morpho-electrical Names used to import the cells from 'cell_data/' -> L1:14 L23:43 L4:46 L5:52 L6:52
+# Create [Morphological,Electrical] = number of cell metype in the sub-pop
+
+with open('../info/anatomy/S1-cells-distributions-Rat.txt') as mtype_file:
+    mtype_content = mtype_file.read()       
+
+cfg.popNumber = {}
+cfg.cellNumber = {} 
+cfg.popLabel = {} 
+popParam = []
+cellParam = []
+cfg.meParamLabels = {} 
+cfg.popLabelEl = {} 
+cfg.cellLabel = {}
+
+for line in mtype_content.split('\n')[:-1]:
+    cellname, mtype, etype, n, m = line.split()
+    metype = mtype + '_' + etype[0:3]
+    cfg.cellNumber[metype] = int(n)
+    cfg.popLabel[metype] = mtype
+    cfg.popNumber[mtype] = int(m)
+    cfg.cellLabel[metype] = cellname
+
+    if mtype not in popParam:
+        popParam.append(mtype)
+        cfg.popLabelEl[mtype] = [] 
+               
+    cfg.popLabelEl[mtype].append(metype)
+    
+    cellParam.append(mtype + '_' + etype[0:3])
+    
+cfg.S1pops = popParam[0:55]
+cfg.S1cells = cellParam[0:207]
+#------------------------------------------------------------------------------  
+# Thalamic Cells
+
+cfg.thalamicpops = ['ss_RTN_o', 'ss_RTN_m', 'ss_RTN_i', 'VPL_sTC', 'VPM_sTC', 'POm_sTC_s1']
+
+cfg.cellNumber['ss_RTN_o'] = int(382 * (210**2/150**2)) # from mouse model (d = 150 um)
+cfg.cellNumber['ss_RTN_m'] = int(382 * (210**2/150**2))
+cfg.cellNumber['ss_RTN_i'] = int(765 * (210**2/150**2))
+cfg.cellNumber['VPL_sTC'] = int(656 * (210**2/150**2))
+cfg.cellNumber['VPM_sTC'] = int(839 * (210**2/150**2))
+cfg.cellNumber['POm_sTC_s1'] = int(685 * (210**2/150**2))
+
+for mtype in cfg.thalamicpops: # No diversity
+	metype = mtype
+	popParam.append(mtype)
+	cfg.popLabel[metype] = mtype
+	cellParam.append(metype)
+
+	cfg.popNumber[mtype] = cfg.cellNumber[metype]
+
+#------------------------------------------------------------------------------  
+cfg.popParamLabels = popParam
+cfg.cellParamLabels = cellParam
+
+#------------------------------------------------------------------------------
+
+with open('/home/fernando/Documents/data_S1_Rat/v7_batch1/v7_batch1_0_0_data.pkl', 'rb') as fileObj: spikesData = pickle.load(fileObj)
+
+spkid = spikesData['simData']['spkid']
+spkt = spikesData['simData']['spkt']
+
+
+#------------------------------------------------------------------------------
+spkTimes = {}
+popID = {}
+N = 0
+for metype in cfg.cellNumber.keys():      
+    for i in range(N,N+cfg.cellNumber[metype]):
+        spkTimes[metype+'_'+str(i)] = []
+    
+    popID[metype] = N
+    N += cfg.cellNumber[metype]
+
+print('N =',N,', Number of spikes =',np.size(spkt),', FR =',np.size(spkt)/(10.0*N))
+
+for mtype in cfg.popNumber.keys():
+    if mtype in cfg.popLabelEl.keys():
+        for metype in cfg.popLabelEl[mtype]:  
+            for i in range(np.size(spkt)):
+                if spkid[i] >= popID[metype] and spkid[i] < popID[metype]+cfg.cellNumber[metype]:
+                    spkTimes[metype+'_'+str(int(spkid[i]))].append(spkt[i])
+    else:
+        metype = mtype
+        for i in range(np.size(spkt)):
+            if spkid[i] >= popID[metype] and spkid[i] < popID[metype]+cfg.cellNumber[metype]:
+                spkTimes[metype+'_'+str(int(spkid[i]))].append(spkt[i])
+
+    print(metype,popID[metype],cfg.cellNumber[metype])
+#------------------------------------------------------------------------------
+cellsTags = []
+for i,metype in enumerate(spikesData['net']['cells']):
+    if i < N:
+        cellsTags2 = {}
+        for tp in ['cellType', 'xnorm', 'ynorm', 'znorm', 'x', 'y', 'z']:
+            cellsTags2[tp] = metype.tags[tp]            
+        cellsTags.append(cellsTags2)
+        print(i,metype)
+#------------------------------------------------------------------------------
+# Save data to pkl file
+import pickle
+with open('../data/spkTimes_v7_batch1.pkl', 'wb') as f:
+    pickle.dump({'spkTimes': spkTimes, 'cellsTags': cellsTags}, f)
