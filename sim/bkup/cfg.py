8e8f9183b9ebd82f4a8e0da445290b9fa29d71e4
fernando <fernandodasilvaborges@gmail.com>
2022-11-16 16:54:19 -0300

    v1000_2022

--- sim/bkup/cfg.py
+++ sim/bkup/cfg.py
@@ -124,8 +124,8 @@ for cellLabel in spkTimes.keys():
     cellsVSName[metype].append('presyn_'+cellLabel)
 
 #------------------------------------------------------------------------------
-cfg.cynradNumber = 1
-cfg.fracmorphoradius = 1.0/2.0
+cfg.cynradNumber = 166
+cfg.fracmorphoradius = 1.0/4.0
 
 excluderadius2a = (cfg.cynradNumber-1)*(0.5*cfg.fracmorphoradius)**2
 excluderadius2b = (cfg.cynradNumber)*(0.5*cfg.fracmorphoradius)**2
@@ -223,17 +223,17 @@ cfg.recordStep = 1.0
 # cfg.recordLFP = [[200, 1100, 200], [220, 1100, 200], [200, 1200, 220], [220, 1200, 220]]
 # cfg.saveLFPPops =  cfg.recordCells 
 
-cfg.recordLFP = [[0, y, 0] for y in [500, 1500, 2000, 2500]] # 4 elec in S1 
+# cfg.recordLFP = [[210, y, 210] for y in [200, 1000, 1200, 1400]] # 1 elec in L1 and 3 elec in L5  
 
 # cfg.saveDipolePops = cfg.S1cells
 
-# cfg.recordDipole = True
-# if cfg.recordDipole: cfg.saveDipoleCells = cfg.S1cells
+cfg.recordDipole = True
+if cfg.recordDipole: cfg.saveDipoleCells = cfg.S1cells
 
 #------------------------------------------------------------------------------
 # Saving
 #------------------------------------------------------------------------------
-cfg.simLabel = 'v1000_batch' + str(cfg.cynradNumber)
+cfg.simLabel = 'v14_batch' + str(cfg.cynradNumber)
 cfg.saveFolder = '../data/'+cfg.simLabel
 # cfg.filename =                	## Set file output name
 cfg.savePickle = True	        	## Save pkl file
@@ -258,13 +258,13 @@ cfg.analysis['plotRaster'] = {'include': cfg.S1cells, 'saveFig': True, 'showFig'
 # cfg.analysis['plotShape'] = {'includePre': cfg.recordCells, 'includePost': cfg.recordCells, 'showFig': False, 'includeAxon': False, 
                             # 'showSyns': False, 'saveFig': True, 'dist': 0.55, 'cvar': 'voltage', 'figSize': (24,12), 'dpi': 600}
 
-cfg.analysis['plotLFP'] = {'plots': ['timeSeries','PSD', 'spectrogram'], 'timeRange': [1000, cfg.duration], 'maxFreq': 400, 'figSize': (8,4), 'saveData': False, 'saveFig': True, 'showFig': False} # 
+# cfg.analysis['plotLFP'] = {'plots': ['timeSeries','PSD', 'spectrogram'], 'electrodes': [[0,1,2,3],[4,5,6,7,8,9,10,11]], 'timeRange': [1000, cfg.duration], 'maxFreq': 400, 'figSize': (8,4), 'saveData': False, 'saveFig': True, 'showFig': False} # 
 
 # cfg.analysis['plotLFP'] = {'separation': 1.0, 'plots': ['timeSeries', 'spectrogram','PSD'], 'timeRange': [0,cfg.duration], 'saveFig': True, 'showFig': False}
 
 cfg.analysis['plot2Dnet']   = {'include': ['presyn_L23_PC_cAD', 'L23_PC_cAD'], 'saveFig': True, 'showConns': False, 'figSize': (24,24), 'view': 'xz', 'fontSize':16}   # Plot 2D cells xz
                  
-# cfg.analysis['plotDipole'] = {'saveFig': True} 
+cfg.analysis['plotDipole'] = {'saveFig': True} 
 
 #------------------------------------------------------------------------------
 # Network 
